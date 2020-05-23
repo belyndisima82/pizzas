dfa1edb2e98a610b7ae53489ccc7031c471f13f7
belyndisima82 <belyndisima@googlemail.com>
2020-05-23 15:22:14 -0500

    heroku password fix

--- connections/mysql.js
+++ connections/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const config = {
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'b636f85a17ad86',
-    password: '1f365401@',
+    password: '1f365401',
     database: 'heroku_ebd610116a92916',
 };
 
