e8cafbfea33837b386d23e94a4686f8c838a6494
belyndisima82 <belyndisima@googlemail.com>
2020-05-27 04:35:42 -0500

    give access control from server

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var ordersController = require("./controllers/ordersController");
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next(); 
+};
+
+app.use(allowCrossDomain);
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({
   type: ["application/json"]
 }));
@@ -14,6 +23,7 @@ app.post("/pizzas", pizzasController.post);
 
 app.get("/orders", ordersController.index);
 app.post("/orders", ordersController.post);
+
 
 app.use(function(err, req, res, next) {
   console.log(err);
@@ -25,6 +35,6 @@ app.use(function(err, req, res, next) {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening at http://localhost:3000/`);
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Listening at http://localhost:3002/`);
 });
